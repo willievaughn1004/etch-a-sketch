b33a47699ba3f64e149abebbf5ea14fb04ad637e
Willie Vaughn <willievaughn1004@gmail.com>
2023-06-28 17:08:11 -0400

    Add clear button, color selection, & grid selecter

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,25 @@
+// Variables
+
 const etchASketch = document.querySelector(".etch-a-sketch");
 
-function makeSquare(size) {
-  let move = false;
+const colorButton = document.querySelector(".color-mode");
+const rainbowButton = document.querySelector(".rainbow-mode");
+const eraserButton = document.querySelector(".eraser");
+const clearButton = document.querySelector(".clear");
 
+const rangeDisplay = document.querySelector(".range-display");
+const rangeSlider = document.querySelector(".range-slider");
+const colorInput = document.querySelector(".color-input");
+
+// Functions
+
+function resizeSquare() {
+  etchASketch.innerHTML = "";
+  makeGrid(rangeSlider.value);
+  rangeDisplay.textContent = `${rangeSlider.value} x ${rangeSlider.value}`;
+}
+
+function makeSquare(size) {
   const newSquare = document.createElement("div");
 
   newSquare.style.width = `calc(100% / ${size})`;
@@ -12,26 +29,36 @@ function makeSquare(size) {
   etchASketch.append(newSquare);
 
   newSquare.addEventListener("mousedown", function () {
-    newSquare.style.backgroundColor = "white";
+    newSquare.style.backgroundColor = `${colorInput.value}`;
     moved = true;
   });
 
   newSquare.addEventListener("mousemove", function () {
     if (moved) {
-      newSquare.style.backgroundColor = "white";
+      newSquare.style.backgroundColor = `${colorInput.value}`;
     }
-  })
+  });
 
   newSquare.addEventListener("mouseup", function () {
     moved = false;
   });
-  
 }
 
 function makeGrid(squares) {
-  for (let i = 4500; i > 0; i--) {
+  for (let i = squares * squares; i > 0; i--) {
     makeSquare(squares);
   }
 }
 
-makeGrid(30);
+resizeSquare();
+
+// Event Listeners
+
+clearButton.addEventListener("click", resizeSquare);
+
+rangeSlider.addEventListener("change", resizeSquare);
+rangeSlider.addEventListener("mousemove", resizeSquare);
+
+eraserButton.addEventListener("click");
+
+// You need to add a slider, as you're not making much progress with prompt.
